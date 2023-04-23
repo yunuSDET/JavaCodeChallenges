2079cf8d4a7fb9e872322d31b958e364e63e8a1b
yunuskulcu84 <yunuskulcu84@gmail.com>
2023-04-24 01:15:21 +0300

    First initial

--- src/arrayLists/Ex7.java
+++ src/arrayLists/Ex7.java
@@ -1,8 +1,20 @@
 package arrayLists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Ex7 {
 
     public static void main(String[] args) {
+//Given an ArrayList of integers,
+// rotate the elements to the right by a given number of steps.
+
+ ArrayList<Integer> numbers=new ArrayList<>(Arrays.asList(3,4,6,1,12,34,65,2));
+    int rotate=6;
+        Collections.rotate(numbers,rotate);
+
+        System.out.println(numbers);
 
 
     }
