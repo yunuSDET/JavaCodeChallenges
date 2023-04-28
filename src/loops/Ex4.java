d460b83d58bb73a87932ab4777cddd2d34d5d456
yunuskulcu84 <yunuskulcu84@gmail.com>
2023-04-28 21:36:12 +0300

    First initial

--- src/loops/Ex4.java
+++ src/loops/Ex4.java
@@ -3,6 +3,7 @@ package loops;
 public class Ex4 {
 
     public static void main(String[] args) {
+
 //Find the sum of the digits of the numbers between 100 and 1000 with using for loops
 
         for (int i = 100; i <1000 ; i++) {
